1f6f092fb9f0586fcb758454e9d68c26c87f4e84
LibLev <libardi.levente@gmail.com>
2020-03-31 09:17:19 +0200

    multiple file uploading with one button is working

--- src/components/product/newProduct.jsx
+++ src/components/product/newProduct.jsx
@@ -9,18 +9,7 @@ class newProduct extends Component {
         productBrand: "",
         productDetails: "",
         productPrice: "",
-        // imgUri: "",
         selectedFiles: null,
-/*      selectedFile1: null,
-        selectedFile2: null,
-        selectedFile3: null,
-        selectedFile4: null,
-        selectedFile5: null,
-        imgUri1: "",
-        imgUri2: "",
-        imgUri3: "",
-        imgUri4: "",
-        imgUri5: "",*/
         imgUris: []
     };
 
@@ -52,55 +41,12 @@ class newProduct extends Component {
         this.setState({imgUris: names})
     };
 
-    /*    fileSelectedHandler1 = event => {
-            this.setState({selectedFile1: event.target.files[0]})
-        };
-
-        fileSelectedHandler2 = event => {
-            this.setState({selectedFile2: event.target.files[0]})
-        };
-
-        fileSelectedHandler3 = event => {
-            this.setState({selectedFile3: event.target.files[0]})
-        };
-
-        fileSelectedHandler4 = event => {
-            this.setState({selectedFile4: event.target.files[0]})
-        };
-
-        fileSelectedHandler5 = event => {
-            this.setState({selectedFile5: event.target.files[0]})
-        };*/
-
     fileUploadHandler = () => {
         axios.post("http://localhost:8080/uploadMultipleFiles", this.state.selectedFiles).then(resp => {
             console.log(resp);
             console.log(this.state.imgUris)
         })
     };
-
-/*    multipleFileUploadHandler = () => {
-        const fd = new FormData;
-        fd.append("images", this.state.selectedFile1, this.state.selectedFile1.name);
-        fd.append("images", this.state.selectedFile2, this.state.selectedFile2.name);
-        fd.append("images", this.state.selectedFile3, this.state.selectedFile3.name);
-        fd.append("images", this.state.selectedFile4, this.state.selectedFile4.name);
-        fd.append("images", this.state.selectedFile5, this.state.selectedFile5.name);
-        axios.post("http://localhost:8080/uploadMultipleFiles", fd).then(resp => {
-            console.log(resp);
-            this.setState(
-                {
-                    imgUris: this.state.imgUris.concat([this.state.selectedFile1.name, this.state.selectedFile2.name,
-                        this.state.selectedFile3.name, this.state.selectedFile4.name, this.state.selectedFile5.name]),
-                    imgUri1: this.state.selectedFile1.name,
-                    imgUri2: this.state.selectedFile2.name,
-                    imgUri3: this.state.selectedFile3.name,
-                    imgUri4: this.state.selectedFile4.name,
-                    imgUri5: this.state.selectedFile5.name
-                });
-            console.log(this.state)
-        })
-    };*/
 
     saveProduct = () => {
         axios.post("http://localhost:8080/saveProduct",
@@ -115,7 +61,7 @@ class newProduct extends Component {
             .then(resp => {
                 console.log(resp)
             });
-       // window.location = "/home";
+        window.location = "/home";
     };
 
     render() {
@@ -126,18 +72,6 @@ class newProduct extends Component {
                         <label htmlFor="exampleFormControlFile1">Képek kiválasztása</label>
                         <input type="file" className="form-control-file" id="exampleFormControlFile1" multiple
                                onChange={this.fileSelectedHandler}/>
-                        {/*<label htmlFor="exampleFormControlFile1">Kép2</label>
-                        <input type="file" className="form-control-file" id="exampleFormControlFile1"
-                               onChange={this.fileSelectedHandler2}/>
-                        <label htmlFor="exampleFormControlFile1">Kép3</label>
-                        <input type="file" className="form-control-file" id="exampleFormControlFile1"
-                               onChange={this.fileSelectedHandler3}/>
-                        <label htmlFor="exampleFormControlFile1">Kép4</label>
-                        <input type="file" className="form-control-file" id="exampleFormControlFile1"
-                               onChange={this.fileSelectedHandler4}/>
-                        <label htmlFor="exampleFormControlFile1">Kép5</label>
-                        <input type="file" className="form-control-file" id="exampleFormControlFile1"
-                               onChange={this.fileSelectedHandler5}/>*/}
                     </div>
                     <button type="submit" onClick={this.fileUploadHandler}>Képek feltöltése</button>
                 </div>
