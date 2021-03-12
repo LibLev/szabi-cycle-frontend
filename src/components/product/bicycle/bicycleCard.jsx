d7e471e4189830eca45d4b8bb36218d8e95b66c2
LibLev <libardi.levente@gmail.com>
2021-03-12 16:27:11 +0100

    main pic has been added

--- src/components/product/bicycle/bicycleCard.jsx
+++ src/components/product/bicycle/bicycleCard.jsx
@@ -20,6 +20,17 @@ class BicycleCard extends Component {
 
     };
 
+    getIndexOfMainPic = () => {
+        let result = 0;
+        for (let i = 0; i < this.props.data.imgUris.length; i++) {
+            if (this.props.data.imgUris[i] === this.props.data.imgUri){
+                result = i;
+                break
+            }
+        }
+        return result;
+    }
+
     render() {
         return (
             <div>
@@ -27,7 +38,7 @@ class BicycleCard extends Component {
                 <div className="col-sm-4" onClick={this.openProductPage} style={{marginTop: "20px"}}>
                     <div className="card" style={{width: "18rem"}}>
                         <img className="card-img-top"
-                             src={`/bicycle/image/download/${this.props.data.id}/0`}
+                             src={`/bicycle/image/download/${this.props.data.id}/${this.getIndexOfMainPic()}`}
                              alt="Card image cap" style={{height: "70%"}}/>
                         <div className="card-body">
                             <p className="card-text"> {this.props.data.name}</p>
